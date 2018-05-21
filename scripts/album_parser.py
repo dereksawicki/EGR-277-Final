d6c3669319470676b6d0b3fc4cce152194de3b51
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-05-21 02:31:52 -0400

    1:1

--- scripts/album_parser.py
+++ scripts/album_parser.py
@@ -3,20 +3,25 @@ import json
 
 output = []
 with open('album_small.json') as json_file:  
-    data = json.load(json_file)['data']
+	data = json.load(json_file)['data']
 
-    for image in data['images']:
-    	i = {
-    		'src': image['link']
-    	}
+	for image in data['images']:
+		i = {
+			'src': image['link']
+		}
 
-    	w = image['width']
-    	h = image['height']
+		w = image['width']
+		h = image['height']
 
-    	i['width'] = 3 if w < h else 4
-    	i['height'] = 4 if w < h else 3
 
-    	output.append(i)
+		if abs(w-h) < 30:
+			i['width'] = 1
+			i['height'] = 1
+		else:
+			i['width'] = 3 if w < h else 4
+			i['height'] = 4 if w < h else 3
+
+		output.append(i)
 
 with open('out.txt', 'w') as out:
 	json.dump(output, out, ensure_ascii=False)
